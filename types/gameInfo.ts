33fe1775098eb47c546786ee8f1a3d498dc7734f
rmoreno-w <rmoreno.w@gmail.com>
2022-12-18 20:40:07 -0300

    🏷️ Game Info Type

---
+++ types/gameInfo.ts
@@ -0,0 +1,15 @@
+import { reviewType } from './review';
+
+export type gameInfoType = {
+    console_id: string;
+    title: string;
+    resume: string;
+    developer: string;
+    genre: string;
+    rating: number;
+    img: string;
+    reviews: Array<reviewType>;
+    createdAt: string;
+    updatedAt: string;
+    id: string;
+};

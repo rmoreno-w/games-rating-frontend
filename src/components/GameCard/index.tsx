a69314c69d23c253b1b938e6fd2afa99c5bd03ef
rmoreno-w <rmoreno.w@gmail.com>
2022-12-19 20:36:02 -0300

    🏷️ Fixing null type errors

--- src/components/GameCard/index.tsx
+++ src/components/GameCard/index.tsx
@@ -11,33 +11,33 @@ export function GameCard({ gameInfo }: gameCardProps) {
     return (
         <article
             className='border border-green-100 rounded-xl grid grid-rows-2 sm:grid-rows-1 sm:grid-cols-3 sm:flex-row gap-4 sm:gap-12 overflow-hidden hover:cursor-pointer'
-            onClick={() => router.push(`/game/${gameInfo.id}`)}
+            onClick={() => router.push(`/game/${gameInfo!.id}`)}
         >
             <div className='h-full relative'>
-                <Image src={gameInfo.img} fill alt='Zelda GB' className='object-fill' />
+                <Image src={gameInfo!.img} fill alt='Zelda GB' className='object-fill' />
             </div>
 
             <div className='px-8 sm:px-0 sm:pr-8 py-4 sm:py-8 flex flex-col gap-4 justify-between sm:col-span-2'>
-                <h2 className='text-center font-bold text-2xl text-green-400'>{gameInfo.title}</h2>
+                <h2 className='text-center font-bold text-2xl text-green-400'>{gameInfo!.title}</h2>
                 <p className='font-light'>
                     <span className='font-bold'>Resumo</span>
                     <span className='font-bold text-green-400 mr-2'> : </span>
-                    {gameInfo.resume}
+                    {gameInfo!.resume}
                 </p>
                 <p className='font-light'>
                     <span className='font-bold'>Desenvolvedor</span>
                     <span className='font-bold text-green-400 mr-2'> : </span>
-                    {gameInfo.developer}
+                    {gameInfo!.developer}
                 </p>
                 <p className='font-light'>
                     <span className='font-bold'>Gênero</span>
                     <span className='font-bold text-green-400 mr-2'> : </span>
-                    {gameInfo.genre}
+                    {gameInfo!.genre}
                 </p>
                 <p className='font-light'>
                     <span className='font-bold'>Avaliação</span>
                     <span className='font-bold text-green-400 mr-2'> : </span>
-                    {gameInfo.rating}
+                    {gameInfo!.rating}
                 </p>
             </div>
         </article>

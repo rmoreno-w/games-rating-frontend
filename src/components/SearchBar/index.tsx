189bc3b54feeb90253404f4ca3d7db44d3c808e9
rmoreno-w <rmoreno.w@gmail.com>
2022-12-19 15:57:58 -0300

    ✨ Autocomplete on Search Input

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,27 +1,44 @@
 import Image from 'next/image';
+import { useRouter } from 'next/router';
 import React, { useEffect, useState } from 'react';
 import useDebounce from '../../../hooks/useDebounce';
+import { apiClient } from '../../../services/axios';
 
 export function SearchBar() {
     const [searchQuery, setSearchQuery] = useState('');
     const [searchOption, setSearchOption] = useState('title');
     const [suggestedResults, setSuggestedResults] = useState<string[]>([]);
     const debouncedSearchQuery = useDebounce(searchQuery, 500);
+    const router = useRouter();
 
     async function apiSearch(e: React.MouseEvent<HTMLButtonElement>) {
-        console.log(`games/find/${searchOption}?${searchOption}=${searchQuery}`);
-        // apiClient.get(`games/find/${searchOption}?${searchOption}=${searchQuery}`)
+        router.push('');
+        apiClient
+            .get(`games/find/autocomplete?${searchOption}=${searchQuery}`)
+            .then((response) => {
+                console.log(response);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
     }
 
-    function apiAutoComplete(e: React.ChangeEvent<HTMLInputElement>) {}
+    function apiAutoComplete() {
+        apiClient
+            .get(`games/find/autocomplete?${searchOption}=${searchQuery}`)
+            .then((response) => {
+                setSuggestedResults(response.data);
+            })
+            .catch((error) => {
+                setSuggestedResults([]);
+            });
+    }
 
     // Effect para fazer Debouncing
     useEffect(
         () => {
             if (debouncedSearchQuery) {
-                apiSearch().then((suggestionResults) => {
-                    console.log(suggestionResults);
-                });
+                apiAutoComplete();
             } else {
                 setSuggestedResults([]);
             }
@@ -31,35 +48,51 @@ export function SearchBar() {
     );
 
     return (
-        <div className='w-3/4 self-center relative grid grid-cols-12 border border-green-50 rounded-2xl focus:outline-none focus:border-green-600 focus:ring-1 focus:ring-green-600 '>
-            <select
-                value={searchOption}
-                onChange={(e) => setSearchOption(e.target.value)}
-                className='p-2 bg-transparent col-span-2'
-            >
-                <option className='bg-black' value='title'>
-                    Título
-                </option>
-                <option className='bg-black' value='developer'>
-                    Desenvolvedor
-                </option>
-                <option className='bg-black' value='genre'>
-                    Gênero
-                </option>
-            </select>
-            <input
-                className='bg-transparent focus:outline-none focus:border-green-600 focus:ring-1 focus:ring-green-600 leading-10 pl-4 col-span-9'
-                value={searchQuery}
-                onChange={(e: React.ChangeEvent<HTMLInputElement>) => setSearchQuery(e.target.value)}
-                placeholder='Procurar por...'
-                type='search'
-            />
-            <button
-                className='relative h-full bg-transparent'
-                onClick={(e: React.MouseEvent<HTMLButtonElement>) => apiSearch(e)}
-            >
-                <Image src='/search.svg' alt='' fill />
-            </button>
+        <div className='w-3/4 self-center flex flex-col items-center'>
+            <div className='relative grid grid-cols-12 border border-green-50 rounded-2xl focus:outline-none focus:border-green-600 focus:ring-1 focus:ring-green-600 '>
+                <select
+                    value={searchOption}
+                    onChange={(e) => setSearchOption(e.target.value)}
+                    className='p-2 bg-transparent col-span-2'
+                >
+                    <option className='bg-black' value='title'>
+                        Título
+                    </option>
+                    <option className='bg-black' value='developer'>
+                        Desenvolvedor
+                    </option>
+                    <option className='bg-black' value='genre'>
+                        Gênero
+                    </option>
+                </select>
+                <input
+                    className='bg-transparent focus:outline-none focus:border-green-600 focus:ring-1 focus:ring-green-600 leading-10 pl-4 col-span-9'
+                    value={searchQuery}
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => setSearchQuery(e.target.value)}
+                    placeholder='Procurar por...'
+                    type='search'
+                />
+                <button
+                    className='relative h-full bg-transparent'
+                    onClick={(e: React.MouseEvent<HTMLButtonElement>) => apiSearch(e)}
+                >
+                    <Image src='/search.svg' alt='' fill />
+                </button>
+            </div>
+            {suggestedResults.length > 1 && searchQuery && (
+                <ul className='w-3/4  border border-l-green-50 border-b-green-50 border-r-green-50 border-t-transparent rounded ml-[8%]'>
+                    {suggestedResults.map((suggestion) => {
+                        return (
+                            <li
+                                className='pl-4 bg-[#00000070] hover:brightness-90 hover:bg-gray-600 hover:cursor-pointer'
+                                onClick={(e) => setSearchQuery(suggestion)}
+                            >
+                                {suggestion}
+                            </li>
+                        );
+                    })}
+                </ul>
+            )}
         </div>
     );
 }

3ca7809bb2d196198ebebbef9026d228a33cd42c
rmoreno-w <rmoreno.w@gmail.com>
2022-12-19 20:30:46 -0300

    🚀 Updating API Prod URL

--- services/axios.ts
+++ services/axios.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const apiClient = axios.create({
-    baseURL: 'http://localhost:8080/api',
+    baseURL: 'https://gamesrating-production.up.railway.app/api',
 });
